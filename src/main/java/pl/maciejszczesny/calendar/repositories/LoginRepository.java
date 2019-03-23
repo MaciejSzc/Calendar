c4ee81194a386c8f86bd7b2bb875f1d63f7b4c02
Maciej <maciej.szczesny.orzesze@gmail.com>
2019-03-23 23:36:37 +0100

    update 0.2

--- src/main/java/pl/maciejszczesny/calendar/repositories/LoginRepository.java
+++ src/main/java/pl/maciejszczesny/calendar/repositories/LoginRepository.java
@@ -1,7 +1,13 @@
 package pl.maciejszczesny.calendar.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pl.maciejszczesny.calendar.entities.LoginEntity;
 
+import java.util.Optional;
+
 public interface LoginRepository extends CrudRepository<LoginEntity, Integer> {
+    @Query(nativeQuery = true, value = "SELECT * FROM `user` WHERE `login`= ?1 AND `password` = ?2")
+    Optional<LoginEntity> getUser(String login, String password);
 }
+
